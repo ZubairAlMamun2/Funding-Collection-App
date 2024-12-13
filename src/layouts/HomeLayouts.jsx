b26dea9c2f81d660166b60b8028d56017ac79dd8
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2024-12-13 19:37:32 +0600

    slider added

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavBar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Banner from '../components/Banner'
 
 const HomeLayouts = () => {
   return (
     <div className='w-11/12 mx-auto'>
         <NavBar />
-        <h1 className='min-h-[60vh]'></h1>
+        <Banner />
         <Footer />
     </div>
   )
