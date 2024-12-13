f69bc67b35dc4b0f0a96d0a234aa06de51e5c34e
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2024-12-13 23:18:35 +0600

    banner section updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="carousel ">
       <div id="slide1" className="carousel-item border-none rounded-lg md:h-[80vh] relative w-full">
         <img
-          src="https://i.ibb.co.com/5rmj1Sw/Slider-2.png"
+          src="https://i.ibb.co.com/0nxvj4h/10-1.jpg"
           className="w-full border-none rounded-lg px-5"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -19,7 +19,7 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item border-none md:h-[80vh] rounded-lg relative w-full">
         <img
-          src="https://i.ibb.co.com/WGJrbG4/Slider-1.png"
+          src="https://i.ibb.co.com/4f2bSvL/10-3.jpg"
           className="w-full border-none rounded-lg px-5"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -33,7 +33,7 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item border-none md:h-[80vh] rounded-lg relative w-full">
         <img className="w-full border-none rounded-lg px-5"
-          src="https://i.ibb.co.com/DY99M4v/Slider-3.png"
+          src="https://i.ibb.co.com/wCjscwL/10-2.png"
           
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
