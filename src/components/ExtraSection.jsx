7eb19231902d9f27786cd2454ce2226cbf0a58b7
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2024-12-15 23:15:40 +0600

    React Awesome Reveal added

--- src/components/ExtraSection.jsx
+++ src/components/ExtraSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Bounce, Fade } from "react-awesome-reveal";
 
 const ExtraSection = () => {
   return (
     <div className='grid grid-cols-2  border-none rounded-lg  gap-5'>
         {/* why you donate section */}
         <div className='col-span-2 md:col-span-2 bg-base-200 mt-5 p-5 card shadow-lg'>
-            <h2 className='text-xl font-semibold text-center mb-2 animate__animated animate__bounce'>Photo Gallary</h2>
+            <Bounce><h2 className='text-xl font-semibold text-center mb-2 animate__animated animate__bounce'>Photo Gallary</h2></Bounce>
             <div className='grid gap-5 grid-cols-4'>
                 <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2' src="https://i.ibb.co.com/2skWDZL/10-14.png" alt="" />
                 <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2 ' src="https://i.ibb.co.com/XjtfTbN/10-13.png" alt="" />
@@ -14,7 +15,7 @@ const ExtraSection = () => {
             </div>
         </div>
         <div className='col-span-2 md:col-span-2 bg-base-200 mt-5 p-5 card shadow-lg'>
-            <h2 className='text-xl font-semibold text-center mb-2'>Why You Donate</h2>
+            <Bounce> <h2 className='text-xl font-semibold text-center mb-2'>Why You Donate</h2></Bounce>
             <p>People donate to help those in need, support meaningful causes, and make a positive impact on society. Donations foster compassion, reduce inequality, and empower communities. By sharing resources, individuals contribute to addressing pressing issues, such as poverty, education, and healthcare, promoting a sense of unity, purpose, and shared humanity.</p>
         </div>
         
