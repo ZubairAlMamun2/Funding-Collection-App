97c34c49dad7b26d202005340d5ae9a5d458af59
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2024-12-15 22:05:36 +0600

    extra section updated

--- src/components/ExtraSection.jsx
+++ src/components/ExtraSection.jsx
@@ -7,9 +7,9 @@ const ExtraSection = () => {
         <div className='col-span-2 md:col-span-2 bg-base-200 mt-5 p-5 card shadow-lg'>
             <h2 className='text-xl font-semibold text-center mb-2 animate__animated animate__bounce'>Photo Gallary</h2>
             <div className='grid gap-5 grid-cols-4'>
-                <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2' src="https://i.ibb.co.com/X4jrMZm/dd-4.png" alt="" />
-                <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2 ' src="https://i.ibb.co.com/XZPdkD7/dd-3.png" alt="" />
-                <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2' src="https://i.ibb.co.com/jHbpwJw/dd-2.png" alt="" />
+                <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2' src="https://i.ibb.co.com/2skWDZL/10-14.png" alt="" />
+                <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2 ' src="https://i.ibb.co.com/XjtfTbN/10-13.png" alt="" />
+                <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2' src="https://i.ibb.co.com/F0d7pdY/10-12.png" alt="" />
                 <img className='w-full md:h-[50vh] border rounded-lg shadow-lg col-span-4 md:col-span-2' src="https://i.ibb.co.com/W0LqqB2/dd-1.png" alt="" />
             </div>
         </div>
